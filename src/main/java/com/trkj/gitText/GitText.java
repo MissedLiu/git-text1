afcecc5814c04ea381f78a2b40b4c1a52808b8c3
liucz <2042975339@qq.com>
2022-09-14 19:45:41 +0800

    second commit

--- src/main/java/com/trkj/gitText/GitText.java
+++ src/main/java/com/trkj/gitText/GitText.java
@@ -3,5 +3,6 @@ package com.trkj.gitText;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("jjjjjjjjjj");
+        System.out.println("j2222222222");
     }
 }
